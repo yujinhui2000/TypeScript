6919200565b7db2a54d5f01fb3e714031c65276f
JasonLamv-t <jasonlamv-t@hotmail.com>
2022-02-16 14:43:28 +0800

    update: enum

---
+++ src/enum/index.ts
@@ -0,0 +1,16 @@
+export enum NumType {
+  One = 1,
+  Two = 2,
+  Three,
+  Zero = 0,
+  Four = One + Three,
+  Five = Four + 1,
+  LessThanSix = Zero | One | Two | Three | Four | Five,
+}
+
+export enum StrType {
+  One = 'One',
+  Two = 'Two',
+}
+
+
